67cf10cb3c1a29d07bad31254cd90cc720210e4b
miranto <mirantohrs@gmail.com>
2024-08-05 20:52:05 -0700

    update
    change every class selector into tag selector in test.js

--- test.js
+++ test.js
@@ -9,82 +9,101 @@ const { randomBytes } = require("crypto");
         headless: false,
         defaltViewport: false,
         userDataDir: "./tmp",
+        timeout: 10000,
     });
 
-    let link = ["https://maps.app.goo.gl/zYHvH97VjxaRd5cv7", "https://maps.app.goo.gl/yNx9t7yBqqjiBayx5", "https://maps.app.goo.gl/eJwT7PLSFvzJSH6i6"];
-    let data = {};
+    let link = [
+        "https://maps.app.goo.gl/zYHvH97VjxaRd5cv7",
+        "https://maps.app.goo.gl/yNx9t7yBqqjiBayx5",
+        "https://maps.app.goo.gl/eJwT7PLSFvzJSH6i6",
+        "https://maps.app.goo.gl/6XKpEUykY5aqgbj17",
+        "https://maps.app.goo.gl/42CKmpoLLKfZDamm6",
+        "https://maps.app.goo.gl/TXCM5E3xBvzF17cn8",
+        "https://maps.app.goo.gl/7HxQwQc1PccqRcem6"
+    ]
 
     const scrappingFunc = async (linkToScrapp) => {
         try {
             const page = await browser.newPage();
             await page.goto(linkToScrapp);
+            await page.waitForNetworkIdle();
 
-            const itemCardHandle = await page.$$("div.w6VYqd div.aIFcqe div.m6QErb.WNBkOb");
-            for (const itemCard of itemCardHandle) {
-                const pageTitle = await page.evaluate(el => el.querySelector("div.lMbq3e h1.DUwDvf")?.textContent, itemCard);
-                const pageRating = await page.evaluate(el => el.querySelector("div.lMbq3e div.LBgpqf div.F7nice > span:nth-child(1) > span")?.textContent, itemCard);
-                const pageReviewCount = await page.evaluate(el => el.querySelector("div.lMbq3e div.LBgpqf div.F7nice > span:nth-child(2) > span")?.textContent, itemCard);
-                const pageBusinessType = await page.evaluate(el => el.querySelector("div.lMbq3e div.LBgpqf > div.skqShb > div:nth-child(2) > span > span > button")?.textContent, itemCard);
+            const body = await page.$("body");
+            const mainContainer = await body.$("div:not([aria-hidden]) #app-container #content-container #pane+div > div > div");
+            const pageContainer = await mainContainer.$("div[role='main']");
 
-                data = { pageTitle, pageRating, pageReviewCount, pageBusinessType };
-            }
+            const pageBasicInfo = await pageContainer.$("div:nth-child(2) > div > div");
+            const pageTitle = await page.evaluate(el => {
+                let titleElement;
+                titleElement = Array.from(el.querySelectorAll("h1"));
+                titleElement = titleElement.pop();
+                return titleElement.textContent;
+            }, pageBasicInfo);
 
-            const itemCoordsCardHandle = await page.$$("div.w6VYqd div.aIFcqe div.m6QErb.WNBkOb div.m6QErb.XiKgde div.RcCsl");
-            const pageCoords = []
-            for (const coordItemCard of itemCoordsCardHandle) {
-                const coord = await page.evaluate((el) => el.querySelector("div.rogA2c")?.textContent, coordItemCard);
-                pageCoords.push(coord);
-
-                data = Object.assign(
-                    {},
-                    { ...data },
-                    { pageCoords }
-                );
-            }
-
-            await page.click("div.yx21af button:nth-child(2)");
-            const pageReviews = [];
-
-            let scrollLoadingIndicator;
-            do {
-                scrollLoadingIndicator = await page.$("div.w6VYqd div.aIFcqe div.m6QErb.WNBkOb > div.m6QErb.kA9KIf div.lXJj5c.Hk4XGb div.qjESne");
-                scrollLoadingIndicator?.scrollIntoView();
-
-                await page.waitForNetworkIdle();
-                await page.waitForSelector("div.w6VYqd div.aIFcqe div.m6QErb.WNBkOb > div.m6QErb.kA9KIf div.m6QErb div.jftiEf");
-                const reviewCard = await page.$$("div.w6VYqd div.aIFcqe div.m6QErb.WNBkOb > div.m6QErb.kA9KIf div.m6QErb div.jftiEf");
-                for (const review of reviewCard) {
-                    const reviewId = await page.evaluate(el => el.querySelector("div.jJc9Ad div.GHT2ce div.MyEned")?.getAttribute("id"), review);
-
-                    const reviewUser = await page.evaluate(el => el.querySelector("div.jJc9Ad > div.GHT2ce.NsCY4 div.WNxzHc.qLhwHc button div.d4r55")?.textContent, review);
-                    const reviewRating = await page.evaluate(el => el.querySelectorAll("div.jJc9Ad div.DU9Pgb span.kvMYJc span.hCCjke.NhBTye.elGi1d")?.length, review);
-                    const reviewDate = await page.evaluate(el => el.querySelector("div.jJc9Ad div.DU9Pgb span.rsqaWe")?.textContent, review);
-
-                    if (await page.$(`div.MyEned#${reviewId} button[data-review-id="${reviewId}"]`)) await page.click(`div.MyEned#${reviewId} button[data-review-id="${reviewId}"]`);
-
-                    const reviewText = await page.evaluate(el => el.querySelector("div.jJc9Ad div.MyEned span.wiI7pd")?.textContent, review);
-                    pageReviews.push({ reviewId, reviewUser, reviewRating, reviewDate, reviewText });
+            const { pageRating, pageBusinessType } = await page.evaluate(el => {
+                let element = el.querySelectorAll("div:nth-child(2) div.fontBodyMedium");
+                return {
+                    pageRating: element[0].querySelector("span span").textContent,
+                    pageBusinessType: element[1]?.querySelector("span:first-child").textContent,
                 }
-            } while (scrollLoadingIndicator);
+            }, pageBasicInfo);
 
-            data = Object.assign(
-                {},
-                { ...data },
-                { pageReviews }
+            const pageCoords = await page.evaluate(el => {
+                let coordsElementcontainer = el.querySelector("div[ve-visible='cf']").parentElement;
+                let coordsElement;
+                coordsElement = Array.from(coordsElementcontainer.querySelectorAll("& > div:not([ve-visible='cf'])"));
+                coordsElement = coordsElement.map(element => element.querySelector(":nth-child(2) .fontBodyMedium"))
+                return coordsElement.map(value => value.textContent);
+            }, pageContainer);
+
+            await page.click(`div[role='tablist'] button[aria-label^='Reviews']`);
+            await page.waitForNetworkIdle();
+            await pageContainer.waitForSelector("& > :nth-child(2) > :last-child");
+
+            const pageReviews = [];
+            const scrollLoadingIndicator = await pageContainer.$("& > :last-child > :last-child");
+            do {
+                scrollLoadingIndicator.scrollIntoView();
+                await page.waitForNetworkIdle();
+            } while (scrollLoadingIndicator && await page.evaluate(el => el.innerHTML, scrollLoadingIndicator));
+
+            await page.waitForNetworkIdle();
+            const reviewCard = await page.$$("div[data-review-id][aria-label]");
+            for (const review of reviewCard) {
+                const reviewId = await page.evaluate(el => el.getAttribute("data-review-id"), review);
+                console.log(reviewId)
+
+                if (await page.$(`div#${reviewId} button`)) await page.click(`div#${reviewId} button:not([data-href])`);
+
+                const reviewText = await page.evaluate((el, reviewId) => el.querySelector(`div#${reviewId} span`)?.textContent, review, reviewId);
+                const reviewUser = await page.evaluate(el => el.querySelector("& > div > div > :nth-child(2) > :last-child button > :first-child").textContent, review);
+                const reviewRating = await page.evaluate(el => el.querySelector("& > div > div > :last-child > :first-child > :first-child").getAttribute("aria-label"), review);
+                const reviewDate = await page.evaluate(el => el.querySelector("& > div > div > :last-child > :first-child > :nth-child(2)").textContent, review);
+
+                pageReviews.push({ reviewId, reviewUser, reviewRating, reviewDate, reviewText });
+            }
+            console.log(reviewCard.length);
+
+            return Object.assign({},
+                {
+                    pageTitle,
+                    pageRating,
+                    pageBusinessType
+                },
+                { pageCoords: [...pageCoords] },
+                { pageReviews: [...pageReviews] }
             );
-
-            page.close();
-
-            return data;
         }
         catch (err) {
             console.error(err);
         }
     }
 
+    //await scrappingFunc(link[6]);
     for (const linkToProcess of link) {
-        fs.writeFileSync(`result-${randomBytes(8).toString("hex")}.json`, JSON.stringify(await scrappingFunc(linkToProcess)), "utf-8");
+        await scrappingFunc(linkToProcess);
+        //fs.writeFileSync(`result-${randomBytes(8).toString("hex")}.json`, JSON.stringify(await scrappingFunc(linkToProcess)), "utf-8");
     }
 
-    browser.close();
+    //browser.close();
 })();
